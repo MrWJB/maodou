f66a215c20267e05f52fb06b8aa8d7524c27daf1
wangjibin <wangjibin@163.com>
2021-05-09 13:46:42 +0800

    整合缓存

---
+++ src/main/java/com/jbw/maodou/redisclustercache/CacheService.java
@@ -0,0 +1,13 @@
+package com.jbw.maodou.redisclustercache;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CacheService {
+
+    @Cacheable(value = "c1")
+    public String getNameById(Integer id) {
+        return "Michael";
+    }
+}
